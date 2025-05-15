b9094201f7d22605df3ba3f5b171b1bf8a8689d4
aasamelon <ayamelody386@gmail.com>
2025-05-15 20:30:34 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
